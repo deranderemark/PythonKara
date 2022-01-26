55207692cdf55956c9a052906fe869aadf280cb7
Mark Steffes <88773933+deranderemark@users.noreply.github.com>
2022-01-26 11:08:52 +0100

    remove old code

--- k18_2_c_4.py
+++ k18_2_c_4.py
@@ -1,17 +1,3 @@
-n = 5
-i = 0
-counter = 0
-
-x = 0
-while x != n:
-    y = 0
-    while y != n:
-        if (y % 2) == 0 and x >= i:
-            world.setLeaf(x, y, True)
-        y = y + 1
-    x = x + 1
-   
-# ALTERNATIVE
 n = 10
 breite = 0
 while breite != n +1 :
