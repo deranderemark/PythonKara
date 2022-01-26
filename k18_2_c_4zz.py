b5afcc3a552246371745cce99c60b8b10445910e
Mark Steffes <88773933+deranderemark@users.noreply.github.com>
2022-01-26 16:49:33 +0100

    k18_2_c_4 von J.R.
    This Code is written by @DerBaum28 ! @deranderemark doesn't really care weather it is working or not

---
+++ k18_2_c_4zz.py
@@ -0,0 +1,10 @@
+n = 3
+
+x = 0
+while x != n +1:
+    y = n  
+    while x <= y:
+        world.setLeaf(y, x, True)
+        hoehe = hoehe - 1
+    breite = breite + 2
+# J.R. ist verantwortlich für dem Inhalt dieser Datei!
