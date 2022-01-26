82e8c3babcd06fbad41a511dd1f2a72184c61c91
DerBaum28 <98456254+DerBaum28@users.noreply.github.com>
2022-01-26 17:16:04 +0100

    Update k18_2_c_4_alt.py
    Here is the version of the 4th picture, using the "for"-function.

--- k18_2_c_4_alt.py
+++ k18_2_c_4_alt.py
@@ -1,1 +1,4 @@
-# Noch nicht verfügbar
+n = 5                 #value determines height and width of our desired triangle
+for height in range(0, n, 1):     #sets the boundries for the maximus heigt of our triangle in single steps
+  for width in range(0, height+1, 2):      #sets the boundries for the width of our triangle, which is dependant on our height and therefore n-variable
+    world.setLeaf(height, width, True)     # places the leafes to form our desired triangle
