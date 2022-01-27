e05634ff8c27732eca76d2e0264a954f869acfc6
Mark Steffes <88773933+deranderemark@users.noreply.github.com>
2022-01-27 08:57:13 +0100

    Update k18_2_c_4_alt.py

--- k18_2_c_4_alt.py
+++ k18_2_c_4_alt.py
@@ -1,1 +1,13 @@
-# Noch nicht verfügbar
+# NOT WORKING: in Development
+
+n = 5
+poslist = []
+
+for x in range(0, n, 1):
+    for y in range(0, n, 1):
+        if (y % 2) == 0:
+            poslist.append([x, y])
+            world.setLeaf(x, y, True)
+
+for pos in poslist:
+    world.setLeaf(pos[0], pos[1], True)
